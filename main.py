94b5eb9871faf0f88e92509fd64a0bcbf9e81015
Konner Finney <konner.business@gmail.com>
2021-10-21 17:28:53 -0700

    Major overhaul of gsearch + RegEx parsing
    Overhauled the gsearch file to include the new googlesearch search function parameters as well as include regular expression parsing. Modularized main.py.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+#!/usr/bin/env python3
 from bs4 import BeautifulSoup
 import tkinter as tk
 from tkinter import filedialog
 import glob
 from gsearch import search_google
+import webbrowser
 
 
-def get_text_from_file(write_file, read_file):
+def get_text_from_file(write_file, read_file) -> None:
 	content = read_file.read()
 	content_list = content.split("\n\n")
 	curr_list = list()
@@ -16,8 +18,9 @@ def get_text_from_file(write_file, read_file):
 	write_file.close()
 	read_file.close()
 
+
 # main function to get directory, start calling other functions
-if __name__ ==  "__main__":
+def run() -> None:
 	print("Starting script")
 	root = tk.Tk()
 	root.withdraw()
@@ -40,9 +43,11 @@ if __name__ ==  "__main__":
 
  
 	get_text_from_file(file2, file1)
+	print("done scraping")
+	webbrowser.open(file_dir + "answers.txt")
 
 
 
 
-	
-    
+if __name__ == "__main__":
+	run()
